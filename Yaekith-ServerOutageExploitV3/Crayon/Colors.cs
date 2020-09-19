358097d19af4d220c9247948e22d57c6d0e34edf
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-09-20 01:44:41 +0200

    Omg

---
+++ Yaekith-ServerOutageExploitV3/Crayon/Colors.cs
@@ -0,0 +1,14 @@
+namespace Crayon
+{
+    internal struct Colors
+    {
+        public const int Black = 30,
+            Red = 31,
+            Green = 32,
+            Yellow = 33,
+            Blue = 34,
+            Magenta = 35,
+            Cyan = 36,
+            White = 37;
+    }
+}
---
+++ Yaekith-ServerOutageExploitV3/Crayon/Decorations.cs
@@ -0,0 +1,10 @@
+namespace Crayon
+{
+    internal struct Decorations
+    {
+        public const int Bold = 1,
+            Dim = 2,
+            Underline = 4,
+            Reversed = 7;
+    }
+}
